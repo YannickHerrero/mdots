680758ea0e07d06005d7f5ac1fd39e6db2135ca9
Yannick Herrero <yannick.herrero@proton.me>
2026-05-25 12:12:32 +0200

    add zsh aliases (BSD ls -G, drop batcat alias)

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,9 @@
+# Text Editors & Files
+alias v="nvim"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ls='ls -G'
+mkcd() { mkdir -p -- "$1" && cd -- "$1"; }
+alias vswap="rm -rf ~/.local/state/nvim/swap/"
+alias fetch="macchina"
